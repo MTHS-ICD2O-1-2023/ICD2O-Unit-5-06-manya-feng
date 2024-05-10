e8ec94c8b36d61a620407d181a84090512619bb0
Manya Feng <159185413+manya-feng@users.noreply.github.com>
2024-05-10 04:13:06 +0000

    change

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Manya All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Manya
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function myButtonClicked() {
+
+  const firstinputNumber = parseInt(document.getElementById("firstinputNumber").value)
+  const secondinputNumber = parseInt(document.getElementById("secondinputNumber").value)
+
+  while (counter < secondinputNumber) {
+    answer = answer + firstinputNumber
+    counter = counter + 1
+  }
+
+  document.getElementById("answer").innerHTML = 
+    firstinputNumber + " x " + secondinputNumber + "=" + answer
+}
